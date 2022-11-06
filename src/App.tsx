69488797cf3d01b2ea5869c94322041815aff7ce
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-11-06 13:27:54 +1100

    toggle lists

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import { useState } from "react";
+import { Button } from "react-bootstrap";
 
 import "./App.css";
 import { CharacterList } from "./components/Character/CharacterList";
+import { EpisodeList } from "./components/Episode/EpisodeList";
 
 function App() {
+  const [page, setPage] = useState<"character" | "episode">("character");
+
+  function togglePage() {
+    if (page === "character") {
+      setPage("episode");
+    } else {
+      setPage("character");
+    }
+  }
+
+  const renderComponent = {
+    character: <CharacterList />,
+    episode: <EpisodeList />,
+  };
+
   return (
-    <div className="App">
-      <CharacterList />
+    <div>
+      <div style={{ position: "fixed", top: 30, left: 30 }}>
+        <Button variant="success" size="lg" onClick={togglePage}>
+          {page === "character" ? "Episódios" : "Personagens"}
+        </Button>
+      </div>
+
+      <div className="App">{renderComponent[page]}</div>
     </div>
   );
 }
